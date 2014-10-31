8023fe8e7f7e51906f7ec6cf6f3abaee5abc1f35
Brandon H <brandon@bc3tech.net>
2014-10-31 04:07:24 +0000

    add band sync app
    git-tfs-id: [https://bc3tech.visualstudio.com/DefaultCollection]$/BC3 Technologies/Chocolatey;C189

---
+++ MicrosoftBandSyncApp/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+Install-ChocolateyPackage -packagename 'MicrosoftBandSyncApp' -filetype msi -url 'http://download.microsoft.com/download/9/D/F/9DFED1DA-65E9-4D68-AC91-9DD68F94EDA6/Microsoft%20Band%20Sync%20Setup.msi' -silentargs '/passive'
