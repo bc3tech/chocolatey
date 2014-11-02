b74f2038d1441ba1eeb6a15a3b36523784fc6376
Brandon H <brandon@bc3tech.net>
2014-11-02 01:47:41 +0000

    Change name of ms band sync app per Chocolatey moderation request
    git-tfs-id: [https://bc3tech.visualstudio.com/DefaultCollection]$/BC3 Technologies/Chocolatey;C190

---
+++ Microsoft-Band-Sync/tools/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+Install-ChocolateyPackage -packagename 'Microsoft-Band-Sync' -filetype msi -url 'http://download.microsoft.com/download/9/D/F/9DFED1DA-65E9-4D68-AC91-9DD68F94EDA6/Microsoft%20Band%20Sync%20Setup.msi' -silentargs '/passive'
