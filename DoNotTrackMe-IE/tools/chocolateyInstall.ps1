5ca494619e1c0ced8f1c760bd6ae68d409562a90
Brandon H <brandon@bc3tech.net>
2015-12-04 15:38:19 +0000

    Updated chocolateyInstall.ps1
    git-tfs-id: [https://bc3tech.visualstudio.com/DefaultCollection]$/BC3 Technologies/Chocolatey;C200

--- DoNotTrackMe-IE/tools/chocolateyInstall.ps1
+++ DoNotTrackMe-IE/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage -packagename 'DoNotTrackMe-IE' -filetype exe -url 'http://releases.abine.com/dntme/ie/3.2.1166/installer.exe' -silentargs '/silent /norestart'
+Install-ChocolateyPackage -packagename 'DoNotTrackMe-IE' -filetype exe -url 'http://releases.abine.com/dntme/ie/5.3.1900-Prod/installer.exe' -silentargs '/silent /norestart'
