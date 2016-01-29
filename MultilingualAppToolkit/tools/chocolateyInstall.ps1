f50e655bbdea7067864c093e6f3b84d0b112db75
Brandon H <brandon@bc3tech.net>
2016-01-29 16:49:49 +0000

    Update to v1563
    git-tfs-id: [https://bc3tech.visualstudio.com/DefaultCollection]$/BC3 Technologies/Chocolatey;C216

--- MultilingualAppToolkit/tools/chocolateyInstall.ps1
+++ MultilingualAppToolkit/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage -packagename 'MultilingualAppToolkit' -filetype msi -url 'https://visualstudiogallery.msdn.microsoft.com/6dab9154-a7e1-46e4-bbfa-18b5e81df520/file/143697/12/MultilingualAppToolkit.msi' -silentargs '/passive'
+Install-ChocolateyPackage -packagename 'MultilingualAppToolkit' -filetype msi -url 'https://visualstudiogallery.msdn.microsoft.com/6dab9154-a7e1-46e4-bbfa-18b5e81df520/file/143697/15/MultilingualAppToolkit.msi' -silentargs '/passive'
