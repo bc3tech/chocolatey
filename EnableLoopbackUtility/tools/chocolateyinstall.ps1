8b4af39ce0ef3df0c5179e7815fb1389f47466a2
Brandon H <brandon@bc3tech.net>
2016-05-18 14:47:00 -0500

    Enable Loopback Utility

---
+++ EnableLoopbackUtility/tools/chocolateyinstall.ps1
@@ -0,0 +1,43 @@
+﻿$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'EnableLoopbackUtility'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://www.fiddler2.com/dl/EnableLoopbackUtility.exe'
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/S'
+
+  softwareName  = 'EnableLoopbackUtility*'
+  checksum      = ''
+  checksumType  = 'md5'
+  checksum64    = ''
+  checksumType64= 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
