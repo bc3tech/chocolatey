d0cdff05e83e3aec8a77c69f25f5fc49b420dba7
Brandon H <brandon@bc3tech.net>
2016-05-20 23:19:14 -0500

    Update to 5.5.1930

--- DoNotTrackMe-IE/tools/chocolateyInstall.ps1
+++ DoNotTrackMe-IE/tools/chocolateyInstall.ps1
@@ -1,1 +1,44 @@
-Install-ChocolateyPackage -packagename 'DoNotTrackMe-IE' -filetype exe -url 'http://releases.abine.com/dntme/ie/5.5.1930/installer.exe' -silentargs '/silent /norestart'
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'Blur for IE'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://releases.abine.com/dntme/ie/5.5.1930/installer.exe'
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+
+  softwareName  = 'blank*'
+  checksum      = ''
+  checksumType  = 'md5'
+  checksum64    = ''
+  checksumType64= 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
