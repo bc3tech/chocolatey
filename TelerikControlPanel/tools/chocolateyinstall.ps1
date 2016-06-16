16745f25d51bbfb2fe56baeacbe2a6118b8cc04f
Brandon H <brandon@bc3tech.net>
2016-06-16 09:38:44 -0500

    use windows compression since 7zip is proving to be problematic with this archive

--- TelerikControlPanel/tools/chocolateyinstall.ps1
+++ TelerikControlPanel/tools/chocolateyinstall.ps1
@@ -3,8 +3,16 @@
 $packageName= 'TelerikControlPanel'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
+# Write-Host $env:AppData
+# Write-Host $MyInvocation.MyCommand.Definition
+
+$compressionVal = $env:chocolateyUseWindowsCompression  
+$env:chocolateyUseWindowsCompression   = 'true'
+
 Get-ChocolateyUnzip "tools\TelerikControlPanelSetup_2016_2_0516_1.exe" $toolsDir
 Write-Host 'Creating desktop shortcut...'
 $desktopFolderLocation = [Environment]::GetFolderPath("Desktop")
 $shortcutPath = [System.IO.Path]::Combine($desktopFolderLocation, "Telerik Control Panel.lnk")
 Install-ChocolateyShortcut $shortcutPath "$($toolsDir)\telerikcontrolpanel.exe" -RunAsAdmin
+
+$env:chocolateyUseWindowsCompression   = $compressionVal
