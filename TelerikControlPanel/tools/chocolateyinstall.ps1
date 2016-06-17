ea0df8efd8806b818aadf3c542c0a779ad88e130
Brandon H <brandon@bc3tech.net>
2016-06-16 22:52:44 -0500

    Update telerik control panel to use 7zip for the unzipping, like vivaldi does

--- TelerikControlPanel/tools/chocolateyinstall.ps1
+++ TelerikControlPanel/tools/chocolateyinstall.ps1
@@ -3,16 +3,9 @@
 $packageName= 'TelerikControlPanel'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-# Write-Host $env:AppData
-# Write-Host $MyInvocation.MyCommand.Definition
+	7z x -aoa -bb0 -bso0 -o"$($toolsDir)" "tools\TelerikControlPanelSetup_2016_2_0516_1.zip"
 
-$compressionVal = $env:chocolateyUseWindowsCompression  
-$env:chocolateyUseWindowsCompression   = 'true'
-
-Get-ChocolateyUnzip "tools\TelerikControlPanelSetup_2016_2_0516_1.exe" $toolsDir
-Write-Host 'Creating desktop shortcut...'
+	Write-Host 'Creating desktop shortcut...'
 $desktopFolderLocation = [Environment]::GetFolderPath("Desktop")
 $shortcutPath = [System.IO.Path]::Combine($desktopFolderLocation, "Telerik Control Panel.lnk")
 Install-ChocolateyShortcut $shortcutPath "$($toolsDir)\telerikcontrolpanel.exe" -RunAsAdmin
-
-$env:chocolateyUseWindowsCompression   = $compressionVal
