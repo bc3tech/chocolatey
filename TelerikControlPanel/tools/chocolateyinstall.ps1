f8ef34b0e58f90946bc36014f12798fa684c90b4
Brandon H <brandon@bc3tech.net>
2016-06-16 23:38:29 -0500

    modifying unzip directory

--- TelerikControlPanel/tools/chocolateyinstall.ps1
+++ TelerikControlPanel/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageName= 'TelerikControlPanel'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-	7z x -aoa -bb0 -bso0 -o"$($toolsDir)" "tools\TelerikControlPanelSetup_2016_2_0516_1.zip"
+	7z x -aoa -bb0 -bso0 -o"$($toolsDir)\$($packageName)" "tools\TelerikControlPanelSetup_2016_2_0516_1.zip"
 
 	Write-Host 'Creating desktop shortcut...'
 $desktopFolderLocation = [Environment]::GetFolderPath("Desktop")
 $shortcutPath = [System.IO.Path]::Combine($desktopFolderLocation, "Telerik Control Panel.lnk")
-Install-ChocolateyShortcut $shortcutPath "$($toolsDir)\telerikcontrolpanel.exe" -RunAsAdmin
+Install-ChocolateyShortcut $shortcutPath "$($toolsDir)\\$($packageName)\telerikcontrolpanel.exe" -RunAsAdmin
