39f5a9ae3cfa2ebbdd2633d6a148b321a38d7e5e
Brandon H <brandon@bc3tech.net>
2016-06-23 10:20:01 -0700

    Update Franz to 3.1.0

--- Franz/tools/chocolateyinstall.ps1
+++ Franz/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= 'franz'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/imprecision/franz-app/releases/download/3.0.1/Franz-win32-ia32-3.0.1.zip'
-$url64      = 'https://github.com/imprecision/franz-app/releases/download/3.0.1/Franz-win32-x64-3.0.1.zip'
+$url        = 'https://github.com/imprecision/franz-app/releases/download/3.1.0/Franz-win32-ia32-3.1.0.zip'
+$url64      = 'https://github.com/imprecision/franz-app/releases/download/3.1.0/Franz-win32-x64-3.1.0.zip'
 $fileType = 'EXE'
 
 $zipInstallDir = Install-ChocolateyZipPackage $packageName $url $toolsDir $url64
