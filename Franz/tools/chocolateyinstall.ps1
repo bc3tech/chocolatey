ab55057493e4cec62c7119277ee5a3bbc7d27363
Brandon H <brandon@bc3tech.net>
2016-06-27 11:12:25 -0700

    fixing blank silentargs error

--- Franz/tools/chocolateyinstall.ps1
+++ Franz/tools/chocolateyinstall.ps1
@@ -5,6 +5,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://github.com/imprecision/franz-app/releases/download/3.1.0/Franz-win32-ia32-3.1.0.zip'
 $url64      = 'https://github.com/imprecision/franz-app/releases/download/3.1.0/Franz-win32-x64-3.1.0.zip'
 $fileType = 'EXE'
+$silentArgs = ''
 
 $zipInstallDir = Install-ChocolateyZipPackage $packageName $url $toolsDir $url64
 
