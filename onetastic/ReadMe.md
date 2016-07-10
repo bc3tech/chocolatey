15ad6480f3ed154137872b98bc4caf1c91d1e155
Brandon H <brandon@bc3tech.net>
2016-07-10 00:30:10 -0500

    Onetastic v3.0.0-pre0

---
+++ onetastic/ReadMe.md
@@ -0,0 +1,1 @@
+﻿https://chocolatey.org/packages/onetastic
---
+++ onetastic/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
+#   $f='c:\path\to\thisFile.ps1'
+#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+
+$packageName= 'onetastic' # arbitrary name for the package, used in messages
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://getonetastic.com/downloadFile&fv=0&file=OnetasticInstaller.x86.zip&agree=1&download=' # download url
+$url64      = '' # 64bit URL here or remove - if installer contains both (very rare), use $url
+
+## Download and unpack a zip file - https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
+Install-ChocolateyZipPackage $packageName $url $toolsDir
+
+## Installer, will assert administrative rights - used by Install-ChocolateyPackage
+## use this for embedding installers in the package when not going to community feed or when you have distribution rights
+## - https://chocolatey.org/docs/helpers-install-chocolatey-install-package
+Install-ChocolateyInstallPackage $packageName -FileType 'EXE' -File "$($toolsDir)\OnetasticInstaller.exe" -validExitCodes @(0) -silentArgs = ''
