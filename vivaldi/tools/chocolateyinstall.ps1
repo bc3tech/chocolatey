0ac8fb088848160444e8f13b4c45fc88c5a375d3
Brandon H <brandon@bc3tech.net>
2016-08-30 08:44:01 -0700

    1.3.551.38

--- vivaldi/tools/chocolateyinstall.ps1
+++ vivaldi/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageName= 'vivaldi'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$downloadedFile = Get-ChocolateyWebFile $packageName "$toolsDir\vivaldi-downloaded.zip" -Url 'https://downloads.vivaldi.com/snapshot/Vivaldi.1.3.551.37.exe' -Url64bit 'https://downloads.vivaldi.com/snapshot/Vivaldi.1.3.551.37.x64.exe'
+$downloadedFile = Get-ChocolateyWebFile $packageName "$toolsDir\vivaldi-downloaded.zip" -Url 'https://downloads.vivaldi.com/stable/Vivaldi.1.3.551.38.exe' -Checksum '8BE15AD7D79F15347352C9929EBCFE695CA403AB' -ChecksumType 'SHA1' -Url64bit 'https://downloads.vivaldi.com/stable/Vivaldi.1.3.551.38.x64.exe' -Checksum64 '7667C36DF9CEC6F337CA4A338C9018ECE7F0C60A' -ChecksumType64 'SHA1'
 
 Write-Host 'Unpacking vivaldi installer like an archive...'
 & 7z x -aoa -bb0 -bso0 -o"$($toolsDir)\$($packageName)" $downloadedFile
