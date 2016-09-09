a4188ad16d4d0524c9f705738f7f57820f7a8207
Brandon H <brandon@bc3tech.net>
2016-09-09 08:30:54 -0700

    Franz 4.0.4

--- Franz/tools/chocolateyinstall.ps1
+++ Franz/tools/chocolateyinstall.ps1
@@ -2,12 +2,12 @@
 
 $packageName= 'franz'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/imprecision/franz-app/releases/download/3.1.1/Franz-win32-ia32-3.1.1.zip'
-$url64      = 'https://github.com/imprecision/franz-app/releases/download/3.1.1/Franz-win32-x64-3.1.1.zip'
+$url        = 'https://github.com/meetfranz/franz-app/releases/download/4.0.4/Franz-win32-ia32-4.0.4.zip'
+$url64      = 'https://github.com/meetfranz/franz-app/releases/download/4.0.4/Franz-win32-x64-4.0.4.zip'
 $fileType = 'EXE'
 $silentArgs = ''
 
-$zipInstallDir = Install-ChocolateyZipPackage $packageName $url $toolsDir $url64
+$zipInstallDir = Install-ChocolateyZipPackage $packageName $url -Checksum "16A4CB546F6841580295907285FBBEA047198617" -ChecksumType "SHA1" $toolsDir $url64 -Checksum64 "F6B59469CBA577C0DA8B477C5D3CB3E41627114F" -ChecksumType64 "SHA1"
 
 $fileInstallLocation = Join-Path $zipInstallDir 'FranzSetup.exe'
 Install-ChocolateyInstallPackage $packageName $fileType $silentArgs "$fileInstallLocation"
