f39ec02e149adae4429dcc31adcded6afcd76f20
Brandon H <brandon@bc3tech.net>
2016-10-24 21:56:49 -0700

    phraseexpress 12.0.105

--- PhraseExpress/PhraseExpress.Install/tools/chocolateyinstall.ps1
+++ PhraseExpress/PhraseExpress.Install/tools/chocolateyinstall.ps1
@@ -4,23 +4,23 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'PhraseExpress.Install'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://www.phraseexpress.com/PhraseExpressSetup.msi'
+$url        = 'http://www.phraseexpress.com/PhraseExpressSetup.exe'
 $url64      = $url
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  fileType      = 'MSI'
+  fileType      = 'exe'
   url           = $url
   url64bit      = $url64
 
-  silentArgs    = "/qn /norestart "
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
   validExitCodes= @(0, 3010, 1641)
 
   softwareName  = 'PhraseExpress.Install*'
-  checksum      = '879DE2AB3A59B420BCB001DC455B577F'
+  checksum      = '0A5E24CE98B4AE431A4A2054906611D2'
   checksumType  = 'md5'
-  checksum64    = '879DE2AB3A59B420BCB001DC455B577F'
+  checksum64    = '0A5E24CE98B4AE431A4A2054906611D2'
   checksumType64= 'md5'
 }
 
--- PhraseExpress/PhraseExpress.Portable/tools/chocolateyinstall.ps1
+++ PhraseExpress/PhraseExpress.Portable/tools/chocolateyinstall.ps1
@@ -16,9 +16,9 @@ $packageArgs = @{
 
 
   softwareName  = 'PhraseExpress.Portable*'
-  checksum      = '6614FAB0075C518C3FBCD3C7B15B095D'
+  checksum      = '10D7E013B7949142890A1C5612A3061E'
   checksumType  = 'md5'
-  checksum64    = '6614FAB0075C518C3FBCD3C7B15B095D'
+  checksum64    = '10D7E013B7949142890A1C5612A3061E'
   checksumType64= 'md5'
 }
 
