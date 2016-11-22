4e1d4827d68f589df67a5cc24d6506bdc4a3e73b
Brandon H <brandon@bc3tech.net>
2016-11-22 08:29:14 -0800

    vivaldi 1.5.658.44

--- vivaldi/tools/chocolateyinstall.ps1
+++ vivaldi/tools/chocolateyinstall.ps1
@@ -1,14 +1,27 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName= 'vivaldi'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-$downloadedFile = Get-ChocolateyWebFile $packageName "$toolsDir\vivaldi-downloaded.zip" -Url 'https://downloads.vivaldi.com/stable/Vivaldi.1.4.589.11.exe' -Checksum '4F23B2294D4E1D98C3BA993C863D159A5FC03B08' -ChecksumType 'SHA1' -Url64bit 'https://downloads.vivaldi.com/stable/Vivaldi.1.4.589.11.x64.exe' -Checksum64 'F8588FA0C26EDA9FFE75AE134B4A9C91E4E25F9A' -ChecksumType64 'SHA1'
-
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+ 
+$packageArgs = @{
+  packageName   = $packageName
+  FileFullPath  = Join-Path $toolsDir 'vivaldi-downloaded.zip'
+ 
+  url           = 'https://downloads.vivaldi.com/stable/Vivaldi.1.5.658.44.exe'
+  checksum      = '93A98C71EBEBFA0E1972BAC7FEBBB394'
+  checksumType  = 'md5' #default is md5, can also be sha1
+ 
+  url64bit      = 'https://downloads.vivaldi.com/stable/Vivaldi.1.5.658.44.x64.exe'
+  checksum64    = '5FC006B19FDD21EEF787CEB6D89829DC'
+  checksumType64= 'md5' #default is checksumType
+}
+ 
+$downloadedFile = Get-ChocolateyWebFile @packageArgs
+ 
 Write-Host 'Unpacking vivaldi installer like an archive...'
 & 7z x -aoa -bb0 -bso0 -o"$($toolsDir)\$($packageName)" $downloadedFile
 Get-ChocolateyUnzip "$($toolsDir)\$($packageName)\vivaldi.7z" "$($toolsDir)\$($packageName)"
-
+ 
 Write-Host 'Creating *.ignore files to avoid making shims for other exes'
 New-Item "$($toolsDir)\$($packageName)\Vivaldi-bin\crash_service.exe.ignore" -type file -force | Out-Null
 New-Item "$($toolsDir)\$($packageName)\Vivaldi-bin\wow_helper.exe.ignore" -type file -force | Out-Null
