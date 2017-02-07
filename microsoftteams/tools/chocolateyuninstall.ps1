eb9e99c9554110876b090012f14d6054c973efe6
Brandon H <brandon@bc3tech.net>
2017-02-07 09:42:24 -0800

    MS Teams v0.7.00.3101

---
+++ microsoftteams/tools/chocolateyuninstall.ps1
@@ -0,0 +1,5 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$file = Join-Path (Join-Path (Join-Path $env:LOCALAPPDATA Microsoft) Teams) Update.exe
+
+Start-ChocolateyProcessAsAdmin -ExeToRun $file -Statements "--uninstall -s"
