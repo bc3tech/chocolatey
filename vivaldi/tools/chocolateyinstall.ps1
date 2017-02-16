ee691c308e328a9eac06f5df032da549fceb667b
Brandon H <brandon@bc3tech.net>
2017-02-16 09:35:22 -0800

    vivaldi v1.7.735.46

--- vivaldi/tools/chocolateyinstall.ps1
+++ vivaldi/tools/chocolateyinstall.ps1
@@ -7,13 +7,13 @@ $packageArgs = @{
   packageName   = $packageName
   FileFullPath  = Join-Path $toolsDir 'vivaldi-downloaded.zip'
  
-  url           = 'https://downloads.vivaldi.com/stable/Vivaldi.1.6.689.40.exe'
-  checksum      = '3D235AF5733C042C09244CC6FDC86021'
-  checksumType  = 'md5' #default is md5, can also be sha1
+  url           = 'https://downloads.vivaldi.com/stable/Vivaldi.1.7.735.46.exe'
+  checksum      = '1E78BFC340412158D78240CF68204E26C6464F64'
+  checksumType  = 'sha1' #default is md5, can also be sha1
  
-  url64bit      = 'https://downloads.vivaldi.com/stable/Vivaldi.1.6.689.40.x64.exe'
-  checksum64    = 'E927C899CD35EA5EDBB96995A402C733'
-  checksumType64= 'md5' #default is checksumType
+  url64bit      = 'https://downloads.vivaldi.com/stable/Vivaldi.1.7.735.46.x64.exe'
+  checksum64    = '8E988F978DAB46B4BB154AC8AE0090A3F4D068D6'
+  checksumType64= 'sha1' #default is checksumType
 }
  
 $downloadedFile = Get-ChocolateyWebFile @packageArgs
