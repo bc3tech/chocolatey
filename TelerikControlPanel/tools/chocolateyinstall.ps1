05b44d9ccf6b2283223325560e808a5bfa4dbbc5
Brandon H <brandon@bc3tech.net>
2017-08-29 15:46:37 -0700

    telerikcontrolpanel v 2017.3.0814.1

--- TelerikControlPanel/tools/chocolateyinstall.ps1
+++ TelerikControlPanel/tools/chocolateyinstall.ps1
@@ -3,9 +3,12 @@
 $packageName= 'TelerikControlPanel'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-	7z x -aoa -bb0 -bso0 -o"$($toolsDir)\$($packageName)" "tools\TelerikControlPanelSetup_2016_2_0516_1.zip"
+	7z x -aoa -bb0 -bso0 -o"$($toolsDir)\$($packageName)" "tools\TelerikControlPanelSetup_2017_3_0814_1.zip"
 
 	Write-Host 'Creating desktop shortcut...'
 $desktopFolderLocation = [Environment]::GetFolderPath("Desktop")
 $shortcutPath = [System.IO.Path]::Combine($desktopFolderLocation, "Telerik Control Panel.lnk")
 Install-ChocolateyShortcut $shortcutPath "$($toolsDir)\\$($packageName)\telerikcontrolpanel.exe" -RunAsAdmin
+
+Write-Host 'Creating *.ignore files to avoid making shims for other exes'
+New-Item "$($toolsDir)\$($packageName)\setup.exe.ignore" -type file -force | Out-Null
