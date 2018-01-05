717816e7d58f0f0785d7929d2d8f4269f2e491a9
Brandon H <brandon@bc3tech.net>
2018-01-05 12:33:50 -0800

    vivaldi 1.13.1008.40

--- vivaldi/tools/chocolateyinstall.ps1
+++ vivaldi/tools/chocolateyinstall.ps1
@@ -4,25 +4,18 @@ $packageName= 'vivaldi'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
  
 $packageArgs = @{
-  packageName   = $packageName
-  FileFullPath  = Join-Path $toolsDir 'vivaldi-downloaded.zip'
- 
-  url           = 'https://downloads.vivaldi.com/stable/Vivaldi.1.12.955.36.exe'
-    checksum     = 'AA9D1BC35535487CA566E8F92F43307900E1CBC2A38A8E09F13706124B048B99'
-  checksumType  = 'sha256' #default is md5, can also be sha1
- 
-    url64bit     = 'https://downloads.vivaldi.com/stable/Vivaldi.1.12.955.36.x64.exe'
-    checksum64   = '5632A956C0D3386E936D711DFA77E12BAB20F4821042EBD2076F71E1AF9ACCCC'
-  checksumType64= 'sha256' #default is checksumType
+    packageName    = $packageName
+    unzipLocation  = $toolsDir
+    fileType       = 'exe'
+       
+    url            = 'https://downloads.vivaldi.com/stable/Vivaldi.1.13.1008.40.exe'
+    silentArgs     = '--vivaldi-silent --do-not-launch-chrome --vivaldi-update'
+    
+    checksum       = 'CE1098DD424B14447FB18817BE77DCF81ADE248C93805BFFB053F605EA972174'
+    checksumType   = 'sha256' #default is md5, can also be sha1
+    url64bit       = 'https://downloads.vivaldi.com/stable/Vivaldi.1.13.1008.40.x64.exe'
+    checksum64     = '5DBD517CD9855ABE1689758C4F926AA50C34C68F4D891FB36336B11D88C590FD'
+    checksumType64 = 'sha256' #default is md5, can also be sha1
 }
- 
-$downloadedFile = Get-ChocolateyWebFile @packageArgs
- 
-Write-Host 'Unpacking vivaldi installer like an archive...'
-& 7z x -aoa -bb0 -bso0 -o"$($toolsDir)\$($packageName)" $downloadedFile
-Get-ChocolateyUnzip "$($toolsDir)\$($packageName)\vivaldi.7z" "$($toolsDir)\$($packageName)"
- 
-Write-Host 'Creating *.ignore files to avoid making shims for other exes'
-New-Item "$($toolsDir)\$($packageName)\Vivaldi-bin\crash_service.exe.ignore" -type file -force | Out-Null
-New-Item "$($toolsDir)\$($packageName)\Vivaldi-bin\wow_helper.exe.ignore" -type file -force | Out-Null
-New-Item "$($toolsDir)\$($packageName)\Vivaldi-bin\update_notifier.exe.ignore" -type file -force | Out-Null
+
+Install-ChocolateyPackage @packageArgs
