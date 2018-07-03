91da4d12535d71ab161d19cffad2d67c444f952b
Brandon H <hurlburb@microsoft.com>
2018-07-02 21:19:17 -0500

    Vivaldi 1.15.1147.52

--- vivaldi/tools/chocolateyinstall.ps1
+++ vivaldi/tools/chocolateyinstall.ps1
@@ -1,20 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName= 'vivaldi'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
  
 $packageArgs = @{
     packageName    = $packageName
-    unzipLocation  = $toolsDir
     fileType       = 'exe'
        
-    url            = 'https://downloads.vivaldi.com/stable/Vivaldi.1.15.1147.47.exe'
+    url            = 'https://downloads.vivaldi.com/stable/Vivaldi.1.15.1147.52.exe'
     silentArgs     = '--vivaldi-silent --do-not-launch-chrome --vivaldi-update'
     
-    checksum       = 'A21AA72C2F17DE7EDADC3CB8CFC23F4A2076E58D689CE1EF1B91E98FB6A6A64A'
+    checksum       = '7AF0506A3002FA2245BC6A5E5CA039FC3B954D6A321060A769574A31FC40C708'
     checksumType   = 'sha256' #default is md5, can also be sha1
-    url64bit       = 'https://downloads.vivaldi.com/stable/Vivaldi.1.15.1147.47.x64.exe'
-    checksum64     = 'AF45B268119475929775DE228C1208CAD04872B95D48643DBBB40D618DFE3AC3'
+    url64bit       = 'https://downloads.vivaldi.com/stable/Vivaldi.1.15.1147.52.x64.exe'
+    checksum64     = 'CE8DBA2F47A67FB6002804DDEEE7A22DAB47DB0053464AFAD7A83B34D0B3B0DB'
     checksumType64 = 'sha256' #default is md5, can also be sha1
 }
 
--- vivaldi/tools/chocolateyuninstall.ps1
+++ vivaldi/tools/chocolateyuninstall.ps1
@@ -1,3 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-Uninstall-ChocolateyZipPackage 'vivaldi'
+$packageName = 'vivaldi'
+ 
+$regKey = Get-ItemProperty -Path 'HKCU:\Software\Vivaldi'
+$packageArgs = @{
+    packageName    = $packageName
+    file           = "$($regKey.UninstallString)"
+    silentArgs     = "$($($regKey.UninstallArguments).Replace('--vivaldi','')) --vivaldi-silent --do-not-launch-chrome"
+    validExitCodes = @(0,19)
+}
+
+Uninstall-ChocolateyPackage @packageArgs 
