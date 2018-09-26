e6c51656cef8a9e5f5563114fddfd8b488203656
Brandon H <brandon@bc3tech.net>
2018-09-26 10:32:52 -0700

    prepping for CI builds for Vivaldi that will support both Snapshot and Release versions

---
+++ vivaldi/pack.ps1
@@ -0,0 +1,39 @@
+Param(
+    # Version # of the build
+    [Parameter(Mandatory = $true)]
+    [string]
+    $version,
+    # 32-bit download URL
+    [Parameter(Mandatory = $true)]
+    [string]
+    $url32,
+    # 32-bit SHA value
+    [Parameter(Mandatory = $true)]
+    [string]
+    $sha32,
+    # 64-bit download URL
+    [Parameter(Mandatory = $true)]
+    [string]
+    $url64,
+    # 64-bit SHA256 value
+    [Parameter(Mandatory = $true)]
+    [string]
+    $sha64,
+    # output directory
+    [Parameter()]
+    [string]
+    $outputdirectory = '.\')
+
+$installFile = Get-ChildItem .\tools\chocolateyinstall.ps1
+
+$content = Get-Content $installFile
+$content = $content -replace [regex]::Escape('$32url$'), $url32
+$content = $content -replace [regex]::Escape('$64url$'), $url64
+$content = $content -replace [regex]::Escape('$32sha$'), $sha32
+$content = $content -replace [regex]::Escape('$64sha$'), $sha64
+
+Write-Host $content
+
+Set-Content $installFile.PSPath -Value $content
+
+choco pack --version $version --out $outputdirectory
--- vivaldi/tools/chocolateyinstall.ps1
+++ vivaldi/tools/chocolateyinstall.ps1
@@ -6,14 +6,14 @@ $packageArgs = @{
     packageName    = $packageName
     fileType       = 'exe'
        
-    url            = 'https://downloads.vivaldi.com/stable-auto/Vivaldi.2.0.1309.29.exe'
+    url            = '$32url$'
     silentArgs     = '--vivaldi-silent --do-not-launch-chrome --vivaldi-update'
     
-    checksum       = 'E35A96F798000D9817E7A1340B9ED8546B2413AB4B780886D4222BE16521DF59'
-    checksumType   = 'sha256' #default is md5, can also be sha1
-    url64bit       = 'https://downloads.vivaldi.com/stable-auto/Vivaldi.2.0.1309.29.x64.exe'
-    checksum64     = '02462BB43379A866F23152791ED40CA99FB303E1E7A83B70B357D25E9587163D'
-    checksumType64 = 'sha256' #default is md5, can also be sha1
+    checksum       = '$32sha$'
+    checksumType   = 'sha256'
+    url64bit       = '$64url$'
+    checksum64     = '$64sha$'
+    checksumType64 = 'sha256'
 }
 
 Install-ChocolateyPackage @packageArgs
