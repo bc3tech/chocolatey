62349e5690d72a68e4540291f1481488f610261b
Brandon H <brandon@bc3tech.net>
2018-11-06 14:05:02 -0800

    update MAT to 4.0.1900

--- MultilingualAppToolkit/tools/chocolateyInstall.ps1
+++ MultilingualAppToolkit/tools/chocolateyInstall.ps1
@@ -1,1 +1,17 @@
-Install-ChocolateyPackage -packagename 'MultilingualAppToolkit' -filetype msi -url 'https://visualstudiogallery.msdn.microsoft.com/6dab9154-a7e1-46e4-bbfa-18b5e81df520/file/143697/15/MultilingualAppToolkit.msi' -silentargs '/passive'
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'MultilingualAppToolkit'
+
+$packageArgs = @{
+    packageName    = $packageName
+    fileType       = 'msi'
+
+    url            = 'http://download.microsoft.com/download/3/2/4/324B517B-F5A3-480A-AAE0-DDB522358E96/MultilingualAppToolkit.msi'
+    silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+    validExitCodes= @(0, 3010, 1641)
+  
+    checksum       = 'D5B6491544E335DB58C8B9762285C72CA29847B71B5F6C47D73056E9745BFA51'
+    checksumType   = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
