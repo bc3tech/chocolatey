3d49d83dfcc4f59ce2169fb055391dcf5b2c6efc
Brandon H <brandon@bc3tech.net>
2019-06-02 16:35:46 -0700

    [TrayStatus] removing 64bit URL ref

--- TrayStatus/TrayStatus.Install/tools/chocolateyinstall.ps1
+++ TrayStatus/TrayStatus.Install/tools/chocolateyinstall.ps1
@@ -5,14 +5,12 @@ $ErrorActionPreference = 'Stop';
 $packageName= 'TrayStatus.Install'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://binaryfortressdownloads.com/Download/BFSFiles/114/TrayStatusSetup-4.0.exe'
-$url64      = $url
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
   url           = $url
-  url64bit      = $url64
 
   validExitCodes= @(0, 3010, 1641)
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
--- TrayStatus/TrayStatus.Portable/tools/chocolateyinstall.ps1
+++ TrayStatus/TrayStatus.Portable/tools/chocolateyinstall.ps1
@@ -4,13 +4,12 @@
 $packageName= 'TrayStatus' 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://binaryfortressdownloads.com/Download/BFSFiles/114/TrayStatus-4.0.zip' 
-$url64      = $url
+
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'ZIP'
   url           = $url
-  url64bit      = $url64
   silentArgs    = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`"" 
   validExitCodes= @(0, 3010, 1641)
   softwareName  = 'TrayStatus*' 
