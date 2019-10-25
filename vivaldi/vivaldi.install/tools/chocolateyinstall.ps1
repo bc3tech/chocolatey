f9790597c5888fdbee99dfc8f8c83d64f491ffef
Brandon H <brandon@bc3tech.net>
2019-10-25 14:39:03 -0700

    [vivaldi] refactor to portable/install pkgs
    Fixes #1

---
+++ vivaldi/vivaldi.install/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'vivaldi'
+
+$packageArgs = @{
+    packageName    = $packageName
+    fileType       = 'exe'
+
+    url            = '$32url$'
+    silentArgs     = '--vivaldi-silent --do-not-launch-chrome --vivaldi-update --system-level'
+    
+    checksum       = '$32sha$'
+    checksumType   = 'sha256'
+    url64bit       = '$64url$'
+    checksum64     = '$64sha$'
+    checksumType64 = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
