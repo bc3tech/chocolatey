e44b1d1a41e5a526eca819a240e9ada059bd6d67
Brandon H <brandon@bc3tech.net>
2019-11-12 08:33:33 -0800

    [AlbumStomp] v2.27 update

--- AlbumStomp/tools/chocolateyinstall.ps1
+++ AlbumStomp/tools/chocolateyinstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
 
     softwareName   = 'AlbumStomp*' 
 
-    checksum       = '2E7DB665D7D4061BF6D32D4A43FFA61139536F2FECA358539F56B73894573989'
+    checksum       = '350C4580BE69F988FB2A8871A9F71F0262288F9D0643CA7441AB7829BC98A4B4'
     checksumType   = 'sha256' #default is md5, can also be sha1, sha256 or sha512
 
     # validExitCodes= @(0, 3010, 1641)
