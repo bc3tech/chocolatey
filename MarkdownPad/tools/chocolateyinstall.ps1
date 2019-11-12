484feaddbd80bb2ace1564610935552d7411b29b
Brandon H <brandon@bc3tech.net>
2019-11-12 08:42:54 -0800

    [MarkdownPad] add SHA

--- MarkdownPad/tools/chocolateyinstall.ps1
+++ MarkdownPad/tools/chocolateyinstall.ps1
@@ -3,23 +3,19 @@
 $packageName= 'MarkdownPad'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'http://markdownpad.com/download/markdownpad2-portable.zip'
-$url64      = ''
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'ZIP'
   url           = $url
-  url64bit      = $url64
 
   silentArgs    = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 
   softwareName  = 'MarkdownPad*'
-  checksum      = ''
+  checksum      = '609B59E3764608B45F8C5D77CD4CF288BB0EE70802A112E16A7DFA15D7464980'
   checksumType  = 'md5'
-  checksum64    = ''
-  checksumType64= 'md5'
 }
 
 Install-ChocolateyZipPackage @packageArgs
