6f528b6dd1922098ef9284e2077492ae7f9b7ec6
slampisko <blu3.fl@gmail.com>
2019-12-06 00:07:12 +0100

    [TrayStatus] update to 4.1 (#4)

--- TrayStatus/TrayStatus.Portable/tools/chocolateyinstall.ps1
+++ TrayStatus/TrayStatus.Portable/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 
 $packageName= 'TrayStatus' 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://binaryfortressdownloads.com/Download/BFSFiles/114/TrayStatus-4.0.zip' 
+$url        = 'https://binaryfortressdownloads.com/Download/BFSFiles/114/TrayStatus-4.1.zip' 
 
 $packageArgs = @{
   packageName   = $packageName
@@ -13,7 +13,7 @@ $packageArgs = @{
   silentArgs    = "/qn /norestart /l*v `"$env:TEMP\chocolatey\$($packageName)\$($packageName).MsiInstall.log`"" 
   validExitCodes= @(0, 3010, 1641)
   softwareName  = 'TrayStatus*' 
-  checksum      = '943386E1506E2D37AC6474545E1438744F21B744BFDD406C937B8D46C1F22238'
+  checksum      = '1DE57303ED8F3D315DB37793D8CC0085066A89B2A344BD56793E0185D0BEFBF3'
   checksumType  = 'sha256' #default is md5, can also be sha1
 }
 
