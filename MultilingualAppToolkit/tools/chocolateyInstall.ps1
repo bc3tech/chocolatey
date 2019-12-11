b1a48d265c36251b0eb8030c603551a2ecea9862
Matthew Davis <MatthewJDavis@users.noreply.github.com>
2019-12-11 15:05:14 -0500

    update checksum for version 4.0.1915

--- MultilingualAppToolkit/tools/chocolateyInstall.ps1
+++ MultilingualAppToolkit/tools/chocolateyInstall.ps1
@@ -10,7 +10,7 @@ $packageArgs = @{
     silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
     validExitCodes= @(0, 3010, 1641)
   
-    checksum       = 'D5B6491544E335DB58C8B9762285C72CA29847B71B5F6C47D73056E9745BFA51'
+    checksum       = 'BD088949AAEA3DA4B56FA176F2FB4844637D88B038704DC081DD607374FDFF44'
     checksumType   = 'sha256'
 }
 
