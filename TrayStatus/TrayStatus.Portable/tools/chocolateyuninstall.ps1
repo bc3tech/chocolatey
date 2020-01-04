fa1687766f87ac04b650560d5c25b356efeac1b1
Brandon H <brandon@bc3tech.net>
2020-01-04 10:06:01 -0800

    [TrayStatus] fixing portable uninstall

--- TrayStatus/TrayStatus.Portable/tools/chocolateyuninstall.ps1
+++ TrayStatus/TrayStatus.Portable/tools/chocolateyuninstall.ps1
@@ -1,4 +1,4 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
 $packageName = 'TrayStatus'
-Uninstall-ChocolateyZipPackage $packageName
+Uninstall-ChocolateyZipPackage $packageName 'TrayStatus-4.2.zip.txt'
