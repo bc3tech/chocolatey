5882e352b80f1f34d91fc3e55e43befa8604c1cd
AdmiringWorm <kim.nordmo@gmail.com>
2020-01-22 19:24:35 +0100

    (vivaldi*) Added guards in cases where vivaldi is uninstalled
    If a user have already uninstalled vivaldi,
    but have not yet uninstalled the package
    it will fail because the registry key do not exist.
    This commit adds a guard for thes edge cases by
    checking if the returned registry is null or empty
    first

--- vivaldi/vivaldi.portable/tools/chocolateyuninstall.ps1
+++ vivaldi/vivaldi.portable/tools/chocolateyuninstall.ps1
@@ -1,14 +1,19 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'vivaldi'
  
 $regKey = Get-ItemProperty -Path 'HKCU:\Software\Vivaldi'
-$packageArgs = @{
-    packageName    = $packageName
-    file           = "$($regKey.UninstallString)"
-                          # Workaround to get around issue with incorrect positive from package validator
-    silentArgs     = "$($($regKey.('Uninstall'+'Arguments')).Replace('--vivaldi','')) --force-uninstall --vivaldi-silent --do-not-launch-chrome"
-    validExitCodes = @(0,19)
-}
 
-Uninstall-ChocolateyPackage @packageArgs 
+if (!$regKey) {
+    Write-Warning "Vivaldi looks to already be uninstalled."
+} else {
+    $packageArgs = @{
+        packageName    = $packageName
+        file           = "$($regKey.UninstallString)"
+                              # Workaround to get around issue with incorrect positive from package validator
+        silentArgs     = "$($($regKey.('Uninstall'+'Arguments')).Replace('--vivaldi','')) --force-uninstall --vivaldi-silent --do-not-launch-chrome"
+        validExitCodes = @(0,19)
+    }
+
+    Uninstall-ChocolateyPackage @packageArgs
+}
