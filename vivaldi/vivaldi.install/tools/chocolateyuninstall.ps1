347dc623b5466105b67932e5b0c4f5337d7b475f
Kim J. Nordmo <kim.nordmo+github@gmail.com>
2020-01-22 21:12:21 +0100

    Add guards and prevent package validator flagging the package (#10)
    * (vivaldi*) Split up the words Uninstall and Argument
    
    As the chocolatey validator do a full
    check of the word InstallArgument
    The uninstall key string is also being
    matched with that. Splitting up the words
    should prevent that from happening
    
    * (vivaldi*) Added guards in cases where vivaldi is uninstalled
    
    If a user have already uninstalled vivaldi,
    but have not yet uninstalled the package
    it will fail because the registry key do not exist.
    This commit adds a guard for thes edge cases by
    checking if the returned registry is null or empty
    first

--- vivaldi/vivaldi.install/tools/chocolateyuninstall.ps1
+++ vivaldi/vivaldi.install/tools/chocolateyuninstall.ps1
@@ -3,11 +3,17 @@
 $packageName = 'vivaldi'
  
 $regKey = Get-ItemProperty -Path 'HKCU:\Software\Vivaldi'
-$packageArgs = @{
-    packageName    = $packageName
-    file           = "$($regKey.UninstallString)"
-    silentArgs     = "$($($regKey.UninstallArguments).Replace('--vivaldi','')) --force-uninstall --vivaldi-silent --do-not-launch-chrome"
-    validExitCodes = @(0,19)
-}
 
-Uninstall-ChocolateyPackage @packageArgs 
+if (!$regKey) {
+    Write-Warning "Vivaldi looks to already be uninstalled"
+} else {
+    $packageArgs = @{
+        packageName    = $packageName
+        file           = "$($regKey.UninstallString)"
+                              # Workaround to get around issue with incorrect positive from package validator
+        silentArgs     = "$($($regKey.('Uninstall'+'Arguments')).Replace('--vivaldi','')) --force-uninstall --vivaldi-silent --do-not-launch-chrome"
+        validExitCodes = @(0,19)
+    }
+
+    Uninstall-ChocolateyPackage @packageArgs
+}
